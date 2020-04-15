8b31e0107ec5c781f04b93017d10a15a0ca628ef
Jeffrey Shively <jeffrey.shively@wwt.com>
2020-04-15 13:31:10 -0400

    Team random update

--- team_random.py
+++ team_random.py
@@ -5,12 +5,17 @@ import random
 
 def main():
     team_list = [1, 1, 2, 3, 3, 4, 4]
-    presentations = ['api', 'rest', 'sdk', 'development', 'mdp', 'docker', 'cicd']
+    presentations = ['api', 'rest', 'sdk', 'development',
+                     'mdp', 'docker', 'cicd']
+    presentation_dict = {}
 
     for presentation in presentations:
         team = random.choice(team_list)
-        print(f"Presentation {presentation} assigned to team {team}")
         team_list.pop(team_list.index(team))
+        presentation_dict.update({presentation: team})
+
+    for key, value in presentation_dict.items():
+        print(f"Presentation {key} assigned to team {value}")
 
 
 if __name__ == "__main__":
